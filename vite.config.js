fedf1da422d77de93f304893962bdd2bd6efaa70
khouloudguellati <guellatikhouloud75@gmail.com>
2026-05-30 19:01:18 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/css/app.css",
-                "resources/css/landing.css",
+                // "resources/css/landing.css",
                 "resources/js/app.js",
             ],
             refresh: true,
